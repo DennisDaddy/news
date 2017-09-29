95a3f9395358e3fc16b249fb54347dcb580717a5
DennisDaddy <softteez95@gmail.com>
2017-09-29 22:59:33 +0300

    generate controller Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def Home
+  end
+
+  def About
+  end
+
+  def News
+  end
+
+  def Entertainment
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get Home" do
+    get pages_Home_url
+    assert_response :success
+  end
+
+  test "should get About" do
+    get pages_About_url
+    assert_response :success
+  end
+
+  test "should get News" do
+    get pages_News_url
+    assert_response :success
+  end
+
+  test "should get Entertainment" do
+    get pages_Entertainment_url
+    assert_response :success
+  end
+
+end
