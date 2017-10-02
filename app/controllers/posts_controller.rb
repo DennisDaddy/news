1ebe332125546f1c9dd1fe7d140c4354de4f19ea
DennisDaddy <softteez95@gmail.com>
2017-10-03 00:32:29 +0300

    create add new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,65 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create,:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.paginate(page: params[:page])
+  end
+
   def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
+    @post = Post.new
   end
 
   def show
+    @post = Post.find(params[:id])
+   
+   
   end
 
+  def create
+  @post = current_user.posts.build(post_params)
+  if @post.save
+  flash[:success] = "Post successfully created!"
+  redirect_to projects_url
+  else
+    flash[:danger] = "Post not created fill all fields!"
+    redirect_to request.referrer || root_url
+  end
+end
+
+
+def edit
+ @post = Post.find(params[:id])
+end
+
+def update
+  
+end
+
+def update
+  @post = Post.find(params[:id])
+  if @post.update_attributes(post_params)
+  flash[:success] = "Post successfully updated!"
+  redirect_to @post
+  else
+  render 'edit'
+  end
+end
+
   def destroy
+    @post.destroy
+    flash[:success] = "Post deleted"
+   redirect_to @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :picture, :category_id)
+  end
+
+  def correct_user
+  @post = current_user.posts.find_by(id: params[:id])
+  redirect_to root_url if @post.nil?
   end
 end
