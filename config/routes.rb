5a43fae2c702ab1fff5726f3b219a083d70bd0a2
DennisDaddy <softteez95@gmail.com>
2017-10-02 11:12:57 +0300

    generate model posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/show'
-
-  get 'posts/destroy'
-
+ 
   root 'pages#home'
 
   get '/about', to: 'pages#about'
@@ -27,6 +16,7 @@ Rails.application.routes.draw do
    resources :users
    resources :account_activations, only: [:edit]
    resources :password_resets,     only: [:new, :create, :edit, :update]
+   resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
