78b01d8ec3c317b4d9e08ece294159f14c9a92ca
DennisDaddy <softteez95@gmail.com>
2017-10-03 02:48:01 +0300

    show all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create,:edit, :update, :destroy]
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
@@ -55,7 +55,7 @@ end
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :picture, :category_id)
+    params.require(:post).permit(:title, :content)
   end
 
   def correct_user
